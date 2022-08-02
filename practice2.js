fcdb4e32d125b3512da58ac1d7a4b9f4ce651bd2
fahimulislam23 <fhim292@gmail.com>
2022-08-02 18:33:21 +0600

    second commit

--- practice2.js
+++ practice2.js
@@ -1,19 +1,21 @@
-var marks = 32;
-
-if(marks >= 80){
+marks = 85;
+if(marks >= 80 && marks <= 100){
     console.log('A grade');
 }
-else if(marks >= 60){
+else if(marks >= 60 && marks <= 79){
     console.log('B grade');
 }
-else if(marks >= 50){
+else if(marks >= 50 && marks <= 59){
     console.log('C grade');
 }
-else if(marks >= 40){
+else if(marks >= 40 && marks <= 49){
     console.log('D grade');
 }
-else if(marks >= 33){
+else if(marks >= 33 && marks <= 39){
     console.log('F grade');
+}
+else if(marks > 100){
+    console.log('invalid number');
 }
 else{
     console.log('Try Next Year. Good Luck.')
